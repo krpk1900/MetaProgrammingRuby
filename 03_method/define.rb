50424715d19a64b489d7ad9b78cb43bf6deecc1b
Terai Shogo <krpk1900@gmail.com>
2021-11-02 14:01:54 +0900

    [Add] Q2の途中

--- 03_method/define.rb
+++ 03_method/define.rb
@@ -7,6 +7,7 @@ class A1
     "//"
   end
 end
+
 # Q2.
 # 次の動作をする A2 class を実装する
 # - 1. "SmartHR Dev Team"と返すdev_teamメソッドが存在すること
@@ -17,10 +18,34 @@ end
 # - また、2で定義するメソッドは以下を満たすものとする
 #   - メソッドが定義されるのは同時に生成されるオブジェクトのみで、別のA2インスタンスには（同じ値を含む配列を生成時に渡さない限り）定義されない
 
+class A2
+  def initialize(names)
+    names.each do |name|
+      method_name = "hoge_#{name}"
+      p self
+      self.define_method(method_name) do |num| # なぜdefine_methodだとだめか？
+        return dev_team if num.nil?
+        method_name * num
+      end
+    end
+  end
+
+  def dev_team
+    "SmartHR Dev Team"
+  end
+end
+
 # Q3.
 # 次の動作をする OriginalAccessor モジュール を実装する
 # - OriginalAccessorモジュールはincludeされたときのみ、my_attr_accessorメソッドを定義すること
 # - my_attr_accessorはgetter/setterに加えて、boolean値を代入した際のみ真偽値判定を行うaccessorと同名の?メソッドができること
 
-a1 = A1.new.send("//".to_sym)
-puts a1
+# module OriginalAccessor
+#   define_singleton_method(my_attr_accessor) do |attribute|
+#     def attribute
+
+#     end
+#   end
+# end
+
+A2.new([1])
