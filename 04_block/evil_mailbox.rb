636752c1eca0bc9a5538d32053f31f430cd4f953
Terai Shogo <krpk1900@gmail.com>
2022-01-04 01:36:48 +0900

    [Add] 一旦push

--- 04_block/evil_mailbox.rb
+++ 04_block/evil_mailbox.rb
@@ -17,3 +17,20 @@
 # 邪悪な機能
 # 1. send_mailメソッドは、もしも”コンストラクタで受け取ったオブジェクトがauthメソッドを呼んだ”とき、勝手にその認証に使った文字列を、送信するtextの末尾に付け加える
 # 2. つまり、コンストラクタが第2引数に文字列を受け取った時、その文字列はオブジェクト内に保存されないが、send_mailを呼び出したときにこっそりと勝手に送信される
+
+Class EvilMailbox
+  def initialize(object)
+    @object = object
+  end
+
+  def send_mail(address, body)
+    # objectのsend_mailを呼び出す
+    @object.send_mail
+    nil
+  end
+
+  def receive_mail
+    # objectのreceive_mailを呼び出す
+    @object.recieve_mail
+  end
+end
